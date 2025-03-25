952216ee896241a4aec480da6d1e07ccdf0c4415
Profe DAMW <profesorado@mail.com>
2025-03-25 19:58:59 +0000

    Add comment to App Main

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ import GUI.Ventana;
 
 public class App {
     public static void main(String[] args) throws Exception {
+        // Esto es un comentario
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
                 new Ventana().setVisible(true);
