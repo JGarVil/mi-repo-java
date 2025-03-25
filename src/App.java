4a1675d0a93dbc2d96d9dc755db58de5f3eba9f6
Profe DAMW <profesorado@mail.com>
2025-03-25 19:57:03 +0000

    Add Gui-Ventana

--- src/App.java
+++ src/App.java
@@ -1,11 +1,11 @@
-//import GUI.Ventana;
+import GUI.Ventana;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        //java.awt.EventQueue.invokeLater(new Runnable() {
-        //    public void run() {
-        //        new Ventana().setVisible(true);
-        //    }
-        //});
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Ventana().setVisible(true);
+            }
+        });
     }
 }
